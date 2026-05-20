a69cb53992fdf31dbc838d8d3efeaaf3aa21d839
Mike Manzano <mike@clicketyclacks.co>
2026-05-20 11:36:35 -0700

    Restore stream dot capsule clipping

--- ios/Clawline/Clawline/Views/Chat/StreamPageDotsView.swift
+++ ios/Clawline/Clawline/Views/Chat/StreamPageDotsView.swift
@@ -301,6 +301,7 @@ struct StreamPageDotsView: View {
             dotRow
                 .frame(width: scrubFieldWidth, height: Self.controlHeight, alignment: .center)
                 .frame(width: controlWidth, height: Self.controlHeight, alignment: .center)
+                .clipShape(Capsule())
                 .frame(maxHeight: .infinity, alignment: .bottom)
         }
         .frame(width: scrubFieldWidth, height: Self.minimumHitTargetHeight, alignment: .bottom)
